72b77b4c9050d623e73049921dc2062acdaf2d76
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-07-25 15:57:48 +0000

    Revert users can only talk with friends.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     session[:chats] ||= []
- 
-    @friends = User.all.where(id: Friendship.all.where(user_id: current_user.id).select("friend_id"))
+    
+    @users = User.all.where.not(id: current_user) 
     begin
       @chats = Chat.includes(:recipient, :messages).find(session[:chats])
     rescue ActiveRecord::RecordNotFound
