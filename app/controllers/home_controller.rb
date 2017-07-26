4259f8a9144082bc64e732dd70209c449ad685b2
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-07-26 13:40:41 +0300

    Users can only talk with friends again.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     session[:chats] ||= []
     
-    @users = User.all.where.not(id: current_user) 
+    @users = User.all.where(id: Friendship.all.where(user_id: current_user.id).select("friend_id"))
     begin
       @chats = Chat.includes(:recipient, :messages).find(session[:chats])
     rescue ActiveRecord::RecordNotFound
